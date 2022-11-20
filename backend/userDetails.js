e53dbc49d2695faf9eef759fbf354dcf45abad52
LydiaBloomfield <lydia.r.bloom@gmail.com>
2022-11-20 12:17:26 -0600

    Configured backend registration and password encryption

---
+++ backend/userDetails.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const UserDetailsSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true },
+    password: String,
+  },
+  {
+    collection: "UserInfo",
+  }
+);
+mongoose.model("UserInfo", UserDetailsSchema);
+
+export default UserDetailsSchema;
